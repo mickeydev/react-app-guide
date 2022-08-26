fca6b5feb3eea93e29da61bfb34a6aa3c7d95f41
Michael Yeboah Frimpong <46923296+mickeydev@users.noreply.github.com>
2022-08-27 00:47:53 +0530

    starting_file_reformated
    Reformated the existing code.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ExpenseItem.css"
+
+const ExpenseItem = () => {
+    return (
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <div>Title</div>
+                <div className="expense-item__price">Amount</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
