976a1fc19000894b507238c8656c08b7200f4783
Michael Yeboah Frimpong <46923296+mickeydev@users.noreply.github.com>
2022-10-22 18:24:22 +0530

    small changes
    ExpenseItem.css, ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+import { Button, Icon } from "react-materialize";
 
 function App() {
-  return (
-    <div className="App">
-      <div>Expenses App</div>
-      <ExpenseItem />
-    </div>
-  );
+   const getStarPlanet = () => {
+      fetch("https://swapi.dev/api/planets/")
+         .then((data) => data.json())
+         .then((d) => d.results)
+         .then((planets) => getAsArray(planets));
+   };
+
+   const getAsArray = (loopObject) => {
+      for (const prop of loopObject) {
+         console.log(prop.name);
+      }
+   };
+
+   return (
+      <div>
+         <Button
+            className="red"
+            floating
+            icon={<Icon>add</Icon>}
+            large
+            node="button"
+            waves="light"
+            onClick={getStarPlanet}
+         />
+      </div>
+   );
 }
 
 export default App;
